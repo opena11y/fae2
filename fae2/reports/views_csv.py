3afe56c4fecb08b57569d59178e8fea54c6eca5c
Adarsh <agarwaladarsh95@gmail.com>
2020-01-11 21:34:24 -0600

    updating docx format for reports

--- fae2/reports/views_csv.py
+++ fae2/reports/views_csv.py
@@ -7,9 +7,10 @@ from django.http import HttpResponse
 
 from fae2.settings import SITE_URL
 from reports.models import WebsiteReport
+import docx
 from docx import Document
-# import pdfkit
-import weasyprint
+from docx.enum.dml import MSO_THEME_COLOR_INDEX
+
 
 def get_implementation_status(impl_status):
     if impl_status in ['C', 'AC', 'AC-MC', 'PI', 'PI-MC', 'NI', 'NI-MC', 'MC']:
@@ -60,38 +61,66 @@ def addMetaData(report_obj, writer, path):
     writer.writerow([])
 
 
-def addDocMetaData(report_obj, doc, path):
-    table = doc.add_table(rows=1, cols=2)
-    table.style = 'Light Shading Accent 1'
-    hdr_cells = table.rows[0].cells
-    hdr_cells[0].text = 'Meta Label'
-    hdr_cells[1].text = 'Meta Value'
+def addDocMetaData(report_obj, doc, path, report='', view=''):
+    doc.add_paragraph('Report Title: ' + report_obj.title)
 
-    row_cells = table.add_row().cells
-    row_cells[0].text = 'Title'
-    row_cells[1].text = report_obj.title
+    p = doc.add_paragraph()
+    p.add_run('URL: ')
+    add_hyperlink(p, report_obj.url, report_obj.url, '327f97', True)
+    p.add_run('     Depth: ' + str(report_obj.depth))
+    p.add_run('     Ruleset: ')
+    add_hyperlink(p, SITE_URL + path + '/ruleset/' + report_obj.ruleset.slug, report_obj.ruleset.title, '327f97', True)
 
-    row_cells = table.add_row().cells
-    row_cells[0].text = 'URL'
-    row_cells[1].text = report_obj.url
+    p = doc.add_paragraph('Pages: ' + str(report_obj.page_count) + '   Page Limit: ' + str(report_obj.max_pages) + '    Non-HTML Files: ' +
+                          str(report_obj.excluded_urls.count()))
+    p.add_run('     ')
+    add_hyperlink(p, SITE_URL + path + '/summary/' + report + '/' + view + '/urls/', 'URL Evaluation Information', '327f97', True)
 
-    row_cells = table.add_row().cells
-    row_cells[0].text = 'Ruleset'
-    row_cells[1].text = report_obj.ruleset.title
 
-    row_cells = table.add_row().cells
-    row_cells[0].text = 'Depth'
-    row_cells[1].text = str(report_obj.depth)
+def add_hyperlink(paragraph, url, text, color, underline):
+    """
+    A function that places a hyperlink within a paragraph object.
 
-    row_cells = table.add_row().cells
-    row_cells[0].text = 'Pages'
-    row_cells[1].text = str(report_obj.page_count)
+    :param paragraph: The paragraph we are adding the hyperlink to.
+    :param url: A string containing the required url
+    :param text: The text displayed for the url
+    :return: The hyperlink object
+    """
 
-    row_cells = table.add_row().cells
-    row_cells[0].text = 'Report URL'
-    row_cells[1].text = SITE_URL + path + '/'
+    # This gets access to the document.xml.rels file and gets a new relation id value
+    part = paragraph.part
+    r_id = part.relate_to(url, docx.opc.constants.RELATIONSHIP_TYPE.HYPERLINK, is_external=True)
 
-    doc.add_paragraph('')
+    # Create the w:hyperlink tag and add needed values
+    hyperlink = docx.oxml.shared.OxmlElement('w:hyperlink')
+    hyperlink.set(docx.oxml.shared.qn('r:id'), r_id, )
+
+    # Create a w:r element
+    new_run = docx.oxml.shared.OxmlElement('w:r')
+
+    # Create a new w:rPr element
+    rPr = docx.oxml.shared.OxmlElement('w:rPr')
+
+    # Add color if it is given
+    if not color is None:
+        c = docx.oxml.shared.OxmlElement('w:color')
+        c.set(docx.oxml.shared.qn('w:val'), color)
+        rPr.append(c)
+
+    # Remove underlining if it is requested
+    if not underline:
+        u = docx.oxml.shared.OxmlElement('w:u')
+        u.set(docx.oxml.shared.qn('w:val'), 'none')
+        rPr.append(u)
+
+    # Join all the xml elements together add add the required text to the w:r element
+    new_run.append(rPr)
+    new_run.text = text
+    hyperlink.append(new_run)
+
+    paragraph._p.append(hyperlink)
+
+    return hyperlink
 
 
 def ReportRulesViewCSV(request, report, view):
@@ -139,26 +168,12 @@ def ReportRulesViewCSV(request, report, view):
 
 
 def ReportRulesViewDocx(request, report, view):
-    document = Document()
-    document.add_heading('Summary of ' + report + '-' + view, 1)
-    document.add_paragraph('\n')
-
     response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
     response['Content-Disposition'] = 'attachment; filename="' + \
                                       request.path.replace('/docx/', '').replace('/', '-').strip('-') + '.docx"'
 
+    # Fetching data
     report_obj = WebsiteReport.objects.get(slug=report)
-
-    addDocMetaData(report_obj, document, request.path.replace('/docx/', ''))
-
-    headers = ['Rule Group', 'Violations', 'Warnings', 'Manual Check', 'Passed', 'N/A', 'Score', 'Status']
-    table = document.add_table(rows=1, cols=len(headers))
-    table.style = 'Light Shading Accent 2'
-    header_cells = table.rows[0].cells
-
-    for i in range(len(headers)):
-        header_cells[i].text = headers[i]
-
     page = False
 
     if report_obj.page_count == 1:
@@ -176,6 +191,117 @@ def ReportRulesViewDocx(request, report, view):
             groups = report_obj.ws_rs_results.all()
         else:
             groups = report_obj.ws_rc_results.all()
+
+    # Document Formation
+    document = Document()
+
+    addDocMetaData(report_obj, document, request.path.replace('/docx/', ''), report, view)
+
+    document.add_heading('Summary', 1)
+    document.add_paragraph('')
+
+    headers = ['Rule Group', 'Violations', 'Warnings', 'Manual Check', 'Passed', 'N/A', 'Score', 'Status']
+
+    # First Table
+    table = document.add_table(rows=1, cols=len(headers))
+    table.style = 'Colorful Grid'
+    header_cells = table.rows[0].cells
+
+    for i in range(len(headers)):
+        header_cells[i].text = headers[i]
+
+    for g in groups:
+        row_cells = table.add_row().cells
+        row_cells[0].text = g.get_title()
+        row_cells[1].text = str(g.rules_violation)
+        row_cells[2].text = str(g.rules_violation)
+        row_cells[3].text = str(g.rules_manual_check)
+        row_cells[4].text = str(g.rules_passed)
+        row_cells[5].text = str(g.rules_na)
+        row_cells[6].text = str(g.implementation_score)
+        row_cells[7].text = get_implementation_status(g.implementation_status)
+
+    row_cells = table.add_row().cells
+    row_cells[0].text = 'All Report Groups'
+    row_cells[1].text = str(report_obj.rules_violation)
+    row_cells[2].text = str(report_obj.rules_violation)
+    row_cells[3].text = str(report_obj.rules_manual_check)
+    row_cells[4].text = str(report_obj.rules_passed)
+    row_cells[5].text = str(report_obj.rules_na)
+    row_cells[6].text = str(report_obj.implementation_score)
+    row_cells[7].text = get_implementation_status(report_obj.implementation_status)
+
+    document.add_paragraph('')
+
+    # Second Table
+    table = document.add_table(rows=1, cols=len(headers))
+    table.style = 'Colorful List'
+    header_cells = table.rows[0].cells
+
+    for i in range(len(headers)):
+        header_cells[i].text = headers[i]
+
+    for g in groups:
+        row_cells = table.add_row().cells
+        row_cells[0].text = g.get_title()
+        row_cells[1].text = str(g.rules_violation)
+        row_cells[2].text = str(g.rules_violation)
+        row_cells[3].text = str(g.rules_manual_check)
+        row_cells[4].text = str(g.rules_passed)
+        row_cells[5].text = str(g.rules_na)
+        row_cells[6].text = str(g.implementation_score)
+        row_cells[7].text = get_implementation_status(g.implementation_status)
+
+    row_cells = table.add_row().cells
+    row_cells[0].text = 'All Report Groups'
+    row_cells[1].text = str(report_obj.rules_violation)
+    row_cells[2].text = str(report_obj.rules_violation)
+    row_cells[3].text = str(report_obj.rules_manual_check)
+    row_cells[4].text = str(report_obj.rules_passed)
+    row_cells[5].text = str(report_obj.rules_na)
+    row_cells[6].text = str(report_obj.implementation_score)
+    row_cells[7].text = get_implementation_status(report_obj.implementation_status)
+
+    document.add_paragraph('')
+
+    # Second Table
+    table = document.add_table(rows=1, cols=len(headers))
+    table.style = 'Colorful Shading'
+    header_cells = table.rows[0].cells
+
+    for i in range(len(headers)):
+        header_cells[i].text = headers[i]
+
+    for g in groups:
+        row_cells = table.add_row().cells
+        row_cells[0].text = g.get_title()
+        row_cells[1].text = str(g.rules_violation)
+        row_cells[2].text = str(g.rules_violation)
+        row_cells[3].text = str(g.rules_manual_check)
+        row_cells[4].text = str(g.rules_passed)
+        row_cells[5].text = str(g.rules_na)
+        row_cells[6].text = str(g.implementation_score)
+        row_cells[7].text = get_implementation_status(g.implementation_status)
+
+    row_cells = table.add_row().cells
+    row_cells[0].text = 'All Report Groups'
+    row_cells[1].text = str(report_obj.rules_violation)
+    row_cells[2].text = str(report_obj.rules_violation)
+    row_cells[3].text = str(report_obj.rules_manual_check)
+    row_cells[4].text = str(report_obj.rules_passed)
+    row_cells[5].text = str(report_obj.rules_na)
+    row_cells[6].text = str(report_obj.implementation_score)
+    row_cells[7].text = get_implementation_status(report_obj.implementation_status)
+
+    document.add_paragraph('')
+
+    # Second Table
+    table = document.add_table(rows=1, cols=len(headers))
+    table.style = 'Dark List'
+    header_cells = table.rows[0].cells
+
+    for i in range(len(headers)):
+        header_cells[i].text = headers[i]
 
     for g in groups:
         row_cells = table.add_row().cells
@@ -202,14 +328,6 @@ def ReportRulesViewDocx(request, report, view):
 
     return response
 
-def ReportRulesViewPDF(request, report, view):
-    # pdf = pdfkit.from_url('http://' + SITE_URL + request.path.replace('/pdf/', '') + '/', False)
-    pdf = weasyprint.HTML('http://' + SITE_URL + request.path.replace('/pdf/', '') + '/').write_pdf()
-
-    response = HttpResponse(pdf, content_type='application/pdf')
-    response['Content-Disposition'] = 'attachment; filename="' + \
-                                      request.path.replace('/pdf/', '').replace('/', '-').strip('-') + '.pdf"'
-    return response
 
 def ReportRulesGroupViewCSV(request, report, view, group):
     response = HttpResponse(content_type='text/csv')
